3c901ceb95ba635f6aecc40faae0c6fe75edafaa
Dumilde Matos <dumildejosematos@gmail.com>
2023-11-12 12:44:00 +0000

    Create service interface

--- BuberBreakfast/Services/BreakfastService.cs
+++ BuberBreakfast/Services/BreakfastService.cs
@@ -6,7 +6,7 @@ namespace BuberBreakfast.Services.Breakfasts;
 
 public class BreakfastService : IBreakfastService{
 
-    private readonly Dictionary<Guid, Breakfast> _breakfasts = new();
+    private static readonly Dictionary<Guid, Breakfast> _breakfasts = new();
 
     public void CreateBreakfast(Breakfast breakfast) {
         _breakfasts.Add(breakfast.Id, breakfast);
